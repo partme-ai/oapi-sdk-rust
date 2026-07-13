c30370aa46b839a18f9ee9849a219667605b2ae1
Loong Wan <teachingai@163.com>
2026-07-13 12:20:12 +0800

    Add service module root

---
+++ src/service/mod.rs
@@ -0,0 +1,6 @@
+//! Typed OpenAPI service modules.
+//!
+//! APIs that do not yet have a typed wrapper remain available through
+//! [`crate::ApiRequest`] and [`crate::Client::execute`].
+
+pub mod im;
